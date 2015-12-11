0ed429be18fc4d00baed6513e3e13c2eab68cce1
Daniel Moore <danjamesmoore@gmail.com>
2015-12-11 10:28:39 -0600

    commit8

---
+++ 8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double userInput;
+            bool info = true;
+            do
+            {
+                try
+                {
+                    userInput = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("enter in a valid nunber");
+                    userInput = 0;
+                    info = true;
+
+
+                }
+            }
+            while (info == true);
+
+
+
+
+
+
+        }
+    }
+}
